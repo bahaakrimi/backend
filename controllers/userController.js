989cb3937176fa0633f67fe772f98f624a6d6cd5
bahaakrimi <bahaakrimi145@gmail.com>
2025-02-13 18:50:58 +0100

    adduser with image

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,6 +17,24 @@ module.exports.addUserClient = async (req,res) => {
 
     }
 }
+module.exports.addUserClientWithImg = async (req,res) => {
+    try{
+
+        const{ username , email , password } = req.body;
+        const roleClient = 'client';
+        const {filename} = req.file
+        const user = await userModel.create({
+            username, email, password, role: roleClient, user_image : filename
+
+        })
+
+        res.status(200).json({user});
+
+    }catch (error) {
+        res.status(500).json({message: error.message});
+
+    }
+}
 module.exports.addUserAdmin = async (req,res) => {
     try{
 
@@ -28,6 +46,52 @@ module.exports.addUserAdmin = async (req,res) => {
         })
 
         res.status(200).json({user});
+
+    }catch (error) {
+        res.status(500).json({message: error.message});
+
+    }
+}
+module.exports.getAllUsers = async (req,res) => {
+    try{
+        const userListe = await userModel.find()
+
+        
+
+        res.status(200).json({userListe});
+
+    }catch (error) {
+        res.status(500).json({message: error.message});
+
+    }
+}
+module.exports.getUsersById = async (req,res) => {
+    try{
+        //const id= req.params.id
+        //console.log(req.params.id)
+        const {id} = req.params
+        const userListe = await userModel.findById(id)
+
+        
+
+        res.status(200).json({userListe});
+
+    }catch (error) {
+        res.status(500).json({message: error.message});
+
+    }
+}
+module.exports.deleteUserById = async (req,res) => {
+    try{
+
+        const {id} = req.params
+
+        const checkIfUserExists = await userModel.findById(id);
+        await userModel.findByIdAndDelete(id)
+
+        
+
+        res.status(200).json("delete");
 
     }catch (error) {
         res.status(500).json({message: error.message});
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/userController');
+const upload = require("../middlewares/uploadFile")
 
 /* GET users listing. */
 router.post('/addUserClient',userController.addUserClient);
 router.post('/addUserAdmin',userController.addUserAdmin);
+router.get('/getAllUsers',userController.getAllUsers);
+router.get('/getUsersById/:id',userController.getUsersById);
+router.get('/deleteUserById/:id',userController.deleteUserById);
+router.post('/addUserClientWithImg',upload.single("user_image"),userController.addUserClientWithImg);
+
+
+
+
+
 
 
 module.exports = router;
